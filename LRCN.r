32edcdd7c6a83f9ee7099951e6bdf383352d81fd
Seng Chan You <applegna@gmail.com>
2018-04-28 12:23:02 +0900

    constructing keras model
    Signed-off-by: Seng Chan You <applegna@gmail.com>

--- LRCN.r
+++ LRCN.r
@@ -27,8 +27,71 @@ epochs = 2
 
 # Data Preparation --------------------------------------------------------
 
+#Constructing keras model
+filters<- 8
+fc.shape<-c(32,32)
 
-# Defining Model ------------------------------------------------------
+CNN3D <- keras::keras_model_sequential()
+
+CNN3D <-layer_input(c(min(image.str$time),input_shape,1)) %>%
+    time_distributed(
+        layer_conv_3d(filters = filters,
+                      input_shape = input_shape,
+                      kernel_size = c(3,3,3)
+                      #,padding = "same",
+                      ,activation = "relu"
+                      #,strides =1
+        )#,
+        #input_shape=c(min(image.str$time),input_shape,1)) %>%
+    )%>%
+    time_distributed(
+        layer_max_pooling_3d(pool_size=c(2,2,2))
+    )%>%
+    time_distributed(
+        layer_flatten()
+    ) %>%
+    time_distributed(
+        layer_dense(units = 128)
+    ) %>%
+    layer_lstm(units = lstm_unit) %>%
+    layer_dense(units=2, activation='softmax')
+
+
+CNN.Fc <- keras::keras_model_sequential()
+
+CNN.Fc <-layer_input(c(fc.shape,1)) %>%
+    layer_conv_2d(filters = 64,
+                  kernel_size = c(4,4),
+                  padding = "same",
+                  activation = "relu"#,
+                  #input_shape = c(fc.shape,1)#,strides =1
+    ) %>%
+    layer_conv_2d(filters = 64,
+                  kernel_size = c(4,4),
+                  padding = "same",
+                  activation = "relu" #,strides =1
+    ) %>%
+    layer_conv_2d(filters = 64,
+                  kernel_size = c(4,4),
+                  padding = "same",
+                  activation = "relu" #,strides =1
+    ) %>%
+    layer_max_pooling_2d(c(2,2)) %>%
+    layer_flatten(128) %>%
+    layer_dense(8, activation='softmax')
+
+LRCN <- layer_concatenate(c(CNN3D,CNN.Fc)) %>%
+    layer_dense(units=2, activation='softmax')
+
+
+
+
+
+
+
+
+
+# OLD  Defining Model ------------------------------------------------------
 model <- keras::keras_model_sequential()
 
 inputs<-keras::layer_input(shape = input_shape)
@@ -92,6 +155,8 @@ CNN.Fc %>%
     layer_dense(64, activation='softmax')
 
 LRCN <- keras::keras_model_sequential()
+
+
 
 LRCN <- CNN3D %>%
     {layer_concatenate(inputs = list(CNN.Fc,.), axis =3)} %>%
--- Preprocessing.r
+++ Preprocessing.r
@@ -1,20 +1,41 @@
 library(oro.nifti)
+library(keras)
+library(dplyr)
 dataFolder<-"/Users/chan/data/ABIDE"
 workFolder<-"/Users/chan/OneDrive/Study/ASD_DL/Result"
 ImageList<-list.files(dataFolder)
 #ImageList[90]
 
-image.str<-data.frame()
+#####################################
+##identify the dimension of the data
+#####################################
+# image.str<-data.frame()
+# for (fMR in ImageList){
+#     single.img<-oro.nifti::readNIfTI(file.path(dataFolder,fMR),reorient=FALSE)
+#     d=data.frame(name=fMR, width= dim(single.img)[1], height = dim(single.img)[2], 
+#                vertical = dim(single.img)[3], time = dim(single.img)[4])
+#     image.str<-rbind(image.str,d)
+# }
+# write.csv(image.str,file.path(workFolder,"img_dim.csv"))
 
-for (fMR in ImageList){
-    single.img<-oro.nifti::readNIfTI(file.path(dataFolder,fMR),reorient=FALSE)
-    d=data.frame(name=fMR, width= dim(single.img)[1], height = dim(single.img)[2], 
-               vertical = dim(single.img)[3], time = dim(single.img)[4])
-    image.str<-rbind(image.str,d)
+image.str<-read.csv(file.path(workFolder,"img_dim.csv"))
+
+hist(image.str$time)
+min(image.str$time)#min dimension of time =  116
+
+
+i<-1
+fMR<-ImageList[i]
+single.img<-oro.nifti::readNIfTI(file.path(dataFolder,fMR),reorient=FALSE)
+
+#slice (maximum time dimesion of 116)
+slice.img<-single.img[,,,1:min(image.str$time)]
+
+dimensions<-dplyr::distinct(data.frame(cbind( image.str$width,
+                                               image.str$height,
+                                               image.str$vertical)))
+if( nrow(input_shape) == 1) {
+    input_shape<-as.integer(dimensions)
+}else  {
+    writeLines("Error: input shape is heterogeneous")
 }
-write.csv(image.str,file.path(workFolder,"img_dim.csv"))
-
-d=dim(single.img) #61 73 61 152
-
-image(1:d[1],1:d[2],single.img[,,11,1],col=gray(0:64/64),
-      xlab="",ylab="")
